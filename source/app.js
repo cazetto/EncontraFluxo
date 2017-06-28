370a4d1df7b280e69faec8b00476cf4aaa2dbc56
André Pesci Cazetto <cazetto.andre@gmail.com>
2017-06-28 08:43:56 -0300

    Correct redirection after login or signup

--- source/app.js
+++ source/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { NativeRouter, Route } from 'react-router-native';
+import { NativeRouter, Route, Redirect } from 'react-router-native';
 
 import PrivateRoute from './components/private-route/PrivateRoute';
 import SideMenuHOC from './components/side-menu/SideMenuHOC';
@@ -11,6 +11,8 @@ import Skills from './screens/app/skills/Skills';
 import Events from './screens/app/events/Events';
 import Dashboard from './screens/app/dashboard/Dashboard';
 
+import { withRouter } from 'react-router';
+
 const App = props => (
   <View style={styles.app}>
     <Header toggleSideMenu={props.toggleSideMenu} title="Header"/>
@@ -19,13 +21,17 @@ const App = props => (
         <PrivateRoute exact path="/skills" component={Skills} />
         <PrivateRoute exact path="/interests" component={Interests} />
         <PrivateRoute exact path="/events" component={Events} />
-        <PrivateRoute exact path="/" component={Dashboard} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/" render={() =>
+          props.location.state && props.location.state.fromSignup ?
+            <Redirect to="/skills"/> :
+            <Redirect to="/dashboard"/> }/>
       </View>
     </NativeRouter>
   </View>
 );
 
-export default SideMenuHOC(App);
+export default SideMenuHOC( withRouter (App) );
 
 const styles = StyleSheet.create({
   app: {
