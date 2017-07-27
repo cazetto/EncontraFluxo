d61006363bdafe834113ac0f0b6a7972f2b6ed4a
André Pesci Cazetto <cazetto.andre@gmail.com>
2017-07-27 07:07:42 -0300

    Adjust

--- source/services/EventService.js
+++ source/services/EventService.js
@@ -6,7 +6,7 @@ const EventService = {
   resource: new api.Tastypie.Resource('evento', { provider: APPLICATION_API_CONFIG.name }),
   resourceInFlux: new api.Tastypie.Resource('evento/no-fluxo', { provider: APPLICATION_API_CONFIG.name }),
   resourceHappening: new api.Tastypie.Resource('evento/acontecendo', { provider: APPLICATION_API_CONFIG.name }),
-  resourceJoin: new api.Tastypie.Resource('colaborar', { provider: APPLICATION_API_CONFIG.name }),
+  resourceContribute: new api.Tastypie.Resource('colaborar', { provider: APPLICATION_API_CONFIG.name }),
   find(data) {
     return this.resource.objects.find(data);
   },
@@ -28,10 +28,10 @@ const EventService = {
     return this.resourceHappening.objects.find(data);
   },
   join(data) {
-    return this.resourceJoin.objects.create(data);
+    return this.resourceContribute.objects.create(data);
   },
   leave(data) {
-    return this.resourceJoin.objects.delete(data);
+    return this.resourceContribute.objects.delete(data);
   },
 }
 
