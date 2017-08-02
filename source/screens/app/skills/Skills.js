72e78b1c406d5cacc4d03ec5804db9ac6d22e342
André Pesci Cazetto <cazetto.andre@gmail.com>
2017-08-02 04:49:14 -0300

    Profile: Modified skills to stay equal to interests screen

--- source/screens/app/skills/Skills.js
+++ source/screens/app/skills/Skills.js
@@ -1,187 +1,137 @@
-// React imports
 import React, { Component } from 'react';
-// React Native imports
 import {
   View,
   ScrollView,
   StyleSheet,
-  TextInput,
-  TouchableOpacity,
   Text,
+  TouchableOpacity,
+  Icon,
+  Switch,
   Dimensions,
   Platform,
 } from 'react-native';
 
-// Other vendors imports
-import update from 'immutability-helper';
-import Icon from 'react-native-vector-icons/Entypo';
-
-// Custom components imports
-import Select from '../../../components/select/Select';
 import TouchableRedirectorWrapper from '../../../components/touchable-redirector-wrapper/TouchableRedirectorWrapper';
-import ItemDistributionList from '../../../components/item-distribution-list/ItemDistributionList';
 
-// Services imports
-import NeighborhoodService from '../../../services/NeighborhoodService';
-import SkillService from '../../../services/SkillService';
 import UserService from '../../../services/UserService';
+import SkillService from '../../../services/SkillService';
 
 export default class Skills extends Component {
+
   state = {
-    availableSkills: [],
-    userSkills: [],
-
-    availableSkills: [],
-    userSkills: [],
-
-    neighborhoods: [],
-    neighborhoodCurrentId: null,
-  }
+    skills: [],
+  };
 
   componentWillMount() {
-    this.fetchNeighborhoods();
-
-    var immediateID = setImmediate(() => {
-      clearImmediate(immediateID);
-      // Authenticated services calls goes here
-      this.fetchUserProfile();
-    });
-  }
-  // FETCHES
-  fetchNeighborhoods() {
-    NeighborhoodService.find()
-    .then(response => {
-      let neighborhoods = response.objects;
-      this.setState({neighborhoods});
-    });
+    this.fetchSkills();
   }
 
   fetchSkills() {
     SkillService.find({ limit: 0 })
-    .then(({objects:availableSkills}) => this.setState({availableSkills}))
-    .catch(error => console.log('Error when fetching skills.'));
+    .then(({objects:skills}) => {
+      this.setState({skills});
+      this.fetchUserProfile();
+    })
+    .catch(error => {});
   }
 
   fetchUserProfile() {
     UserService.get()
-    .then(response => {
-      this.setState({userSkills: response.habilidades, neighborhoodCurrentId:response.bairro_id});
-      this.fetchSkills();
+    .then(({habilidades:userSkills}) => {
+      var skills = this.state.skills.map(skill => {
+        skill.selected = userSkills.some(userSkill => skill.id == userSkill.id);
+        return skill;
+      });
+      this.setState({skills});
     })
-    .catch(error => console.log('Error when fetching user profile data.'))
-  }
-
-  // HANDLES
-  onNeighborhoodSelectHandle(neighborhood) {
-    let { id:bairro_id } = neighborhood;
-    UserService.update({bairro_id})
-    .then(response => {})
     .catch(error => {});
   }
 
-  onChangeSkillsHandle(addedUserSkills) {
-    UserService.update({
-      habilidades: addedUserSkills,
-    })
+  removeSkill() {
+  }
+
+  selectItem(index, value) {
+    console.log(value);
+    let skills = this.state.skills.slice();
+    let current = skills[index].selected = value;
+    this.setState({skills});
+
+    let userSkills = skills.filter(skill => {
+      return skill.selected;
+    });
+
+    UserService.update({habilidades: userSkills})
     .then(response => {
-      console.log('onChangeSkillsHandle:UserService:response', response);
+      console.log(response);
     })
-    .catch(error => console.log('Error when update skills in user profile.'))
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  renderSkills() {
+    return this.state.skills.map((skill, index) => {
+      return (
+        <View style={styles.listItem} key={index}>
+          <Text style={styles.title}>{skill.nome}</Text>
+          <Switch
+            onValueChange={value => this.selectItem(index, value)}
+            value={ skill.selected } />
+        </View>
+      );
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.control}>
-          <Text style={styles.inputLabel}>Local onde você mora:</Text>
-          <Select placeholder="SELECIONE O BAIRRO" options={this.state.neighborhoods} defaultSelectedId={this.state.neighborhoodCurrentId} onSelect={this.onNeighborhoodSelectHandle.bind(this)}></Select>
-          <Text style={styles.inputLabel}>Suas habilidades:</Text>
-          <ItemDistributionList
-            available={this.state.availableSkills}
-            added={this.state.userSkills}
-            onAddedItemsChanged={added => this.onChangeSkillsHandle(added)}
-          />
+        <View style={styles.listHeader}>
+          <Text style={styles.listHeaderLabel}>SELECIONE SUAS HABILIDADES</Text>
         </View>
+        <ScrollView>
+          {this.renderSkills()}
+        </ScrollView>
+
         <TouchableRedirectorWrapper path="/app/interests" content={
           <View style={styles.btnActionDone}>
             <Text style={styles.btnActionDoneText}>CONTINUAR</Text>
           </View>
         } />
+
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
   },
-
-  control: {
-    height: Dimensions.get('window').height - (Platform.OS === 'ios' ? 108 : 116),
-    marginHorizontal: 10,
+  listHeader: {
+    paddingHorizontal: 14,
+    paddingTop: 20,
+    paddingBottom: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ECEFF1',
+    backgroundColor: '#F5F5F5',
   },
-
-  inputLabel: {
-    marginTop: 10,
-    marginBottom: 4,
-    marginLeft: 12,
+  listHeaderLabel: {
+    fontSize: 14,
     color: '#757575',
+    textAlign: 'center',
   },
-  input: {
-    height: 36,
-    padding: 10,
-    fontSize: 16,
-    backgroundColor: '#FAFAFA',
-    color: '#616161',
-  },
-
-  addSkillTouchable: {
-    position: 'absolute',
-    right: 0,
-    width: 44,
-  },
-
-  addSkillIcon: {
-    position: 'absolute',
-    right: 0,
-    fontSize: 20,
-    backgroundColor: '#FAFAFA',
-    color: '#BF360C',
-    width: 40,
-    paddingTop: 8,
-    paddingBottom: 5,
-    paddingHorizontal: 10,
-  },
-
-  list: {
-    marginTop: 10,
-    borderTopWidth: 1,
-    borderTopColor: '#ECEFF1',
-  },
-
   listItem: {
-    paddingVertical: 14,
-    paddingLeft: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#ECEFF1',
     backgroundColor: '#FAFAFA',
   },
-
-  itemRemoveIconWrapper: {
-    position: 'absolute',
-    right: 20,
-    marginTop: 10,
+  title: {
+    color: '#757575',
   },
-
-  itemRemoveIcon: {
-    marginTop: 3,
-    fontSize: 20,
-    color: '#b71c1c'
-  },
-
   btnActionDone: {
     backgroundColor: '#455A64',
     padding: 8,
@@ -189,12 +139,11 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 4,
     borderBottomRightRadius: 4,
   },
-
   btnActionDoneText: {
     textAlign: 'center',
     color: '#FFF',
     fontSize: 15,
     padding: 4
-  }
+  },
 
 });
